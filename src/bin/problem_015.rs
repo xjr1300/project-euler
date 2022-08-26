2bdc3b0298ba47253f6ae9ce244176f7e3bc87b9
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-08-26 18:29:48 +0900

    Problem 15

--- src/bin/problem_015.rs
+++ src/bin/problem_015.rs
@@ -13,34 +13,6 @@
 ///     ただし、この変換は正方形のグリッドでしか適用できない。
 /// よって、40回の移動のうち、20回の移動を取る組み合わせ問題である。
 
-fn lattice_path(size: usize) -> u64 {
-    // グリッドの頂点数分のベクタを準備
-    let mut grid: Vec<Vec<u64>> = Vec::new();
-    for _ in 0..=size {
-        grid.push(vec![0u64; size + 1]);
-    }
-    grid[0][0] = 1u64;
-
-    let mut queue: Vec<(usize, usize)> = vec![(0, 0)];
-    while !queue.is_empty() {
-        let current = queue.pop().unwrap();
-        if current.0 < size {
-            if !queue.contains(&(current.0 + 1, current.1)) {
-                queue.push((current.0 + 1, current.1));
-            }
-            grid[current.0 + 1][current.1] += grid[current.0][current.1];
-        }
-        if current.1 < size {
-            if !queue.contains(&(current.0, current.1 + 1)) {
-                queue.push((current.0, current.1 + 1));
-            }
-            grid[current.0][current.1 + 1] += grid[current.0][current.1];
-        }
-    }
-
-    grid[size][size]
-}
-
 fn main() {
     let mut result = 1u64;
     for i in 1..=20 {
@@ -48,5 +20,4 @@ fn main() {
         result /= i;
     }
     println!("{}", result);
-    println!("lattice = {}", lattice_path(20));
 }
