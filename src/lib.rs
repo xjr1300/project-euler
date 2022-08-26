a5571e5ba213f951d7baf6cc7f3c2081762d4761
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-08-26 16:18:00 +0900

    Problem 13

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,7 @@ macro_rules! measure {
         let start = Instant::now();
         let result = $x;
         let end = start.elapsed();
-        println!(
-            "elapsed: {}.{:03}s",
-            end.as_secs(),
-            end.subsec_nanos() / 1_000_000
-        );
+        println!("elapsed: {}.{:03}s", end.as_secs(), end.subsec_millis(),);
         result
     }};
 }
